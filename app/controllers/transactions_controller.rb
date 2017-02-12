bab11d96d52b01376614e40ea55daac5f3ce0275
MVanderwood <m.b.vanderwood@gmail.com>
2017-02-11 22:22:27 -0600

    Basic Stripe transaction integration

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,5 +3,25 @@ class TransactionsController < ApplicationController
   end
 
   def create
+    @amount = 500 # cents
+    
+    customer = Stripe::Customer.create customer_params
+    
+    Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: "Rails Stripe customer",
+      currency: "usd"
+    )
+    
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_transaction_path
+  end
+  
+  private
+  
+  def customer_params
+    params.permit(:stripe_email, :source)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
 Rails.application.routes.draw do
-  get 'transactions/new'
-
-  get 'transactions/create'
-
-  get 'product/show'
-
-  get 'product/index'
-
-  get 'product/new'
-
-  get 'product/create'
-
-  get 'product/edit'
-
-  get 'product/update'
-
-  get 'product/delete'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
+  resources :transactions, only: [:new, :create]
 end
